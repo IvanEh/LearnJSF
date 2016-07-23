b724b5d02d188a07ec330c055c604e46a24f6678
ivaneh <ivanehreshi@gmail.com>
2016-07-23 18:13:01 +0300

    Add command button to the page and demonstrate bean instantiation

--- src/main/java/com/gmail/at/ivanehreshi/HelloWorld.java
+++ src/main/java/com/gmail/at/ivanehreshi/HelloWorld.java
@@ -4,11 +4,19 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "helloWorld", eager = true)
 public class HelloWorld {
+    // counter will never reach above 2
+    // because the bean instantiated every time the page is requested
+    int counter = 1;
+
     public HelloWorld() {
         System.out.println("HelloWorld started!");
     }
 
     public String getMessage() {
-        return "Hello World!";
+        return "Hello World! Counter = " + counter++;
+    }
+
+    public String action() {
+        return "empty";
     }
 }
